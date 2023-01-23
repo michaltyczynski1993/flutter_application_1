57c708e1be1c574170ff9bf1ed7d04428454959b
Michal Tyczynski <m.tyczynski1993@gmail.com>
2023-01-23 19:55:48 +0100

    lists in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,38 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('List elements below'),
+        ),
+        body: getListView(),
+      ),
     );
   }
+}
+
+Widget getListView() {
+  var listView = ListView(
+    children: [
+      ListTile(
+        leading: Icon(Icons.landscape),
+        title: Text('List item'),
+        subtitle: Text('Beautiful View'),
+        trailing: Icon(Icons.wb_sunny),
+        onTap: () {
+          print('you clicked on this icon');
+        },
+      ),
+      ListTile(
+        leading: Icon(Icons.laptop_chromebook),
+        title: Text('Windows'),
+      ),
+      ListTile(
+        leading: Icon(Icons.car_rental),
+        title: Text('Car rental'),
+      )
+    ],
+  );
+
+  return listView;
 }
